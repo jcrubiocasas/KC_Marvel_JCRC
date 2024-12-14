7e295eecfb7a68a527e2bf2963d7c0ab396db067
jcrubiocasas <jcrubio@equinsa.es>
2024-12-14 17:58:53 +0100

    Initial Commit

---
+++ KC_Marvel_JCRC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KC_Marvel_JCRC
+//
+//  Created by Juan Carlos Rubio Casas on 14/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KC_Marvel_JCRC/KC_Marvel_JCRCApp.swift
@@ -0,0 +1,17 @@
+//
+//  KC_Marvel_JCRCApp.swift
+//  KC_Marvel_JCRC
+//
+//  Created by Juan Carlos Rubio Casas on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct KC_Marvel_JCRCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
